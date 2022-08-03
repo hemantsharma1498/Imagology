74a6c8510b045dec1867cd2467088bba8547b5a0
hemantsharma1498 <hemantsharma1498@gmail.com>
2022-08-03 21:55:19 +0530

    finished testing functionality

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -49,4 +49,75 @@ describe('When logged in', async()=>{
         });
     });
 
+    describe('when input is valid', async()=>{
+        beforeEach(async()=>{
+            await page.type('.title input', 'Test title');
+            await page.type('.content input', 'Test content');
+
+            await page.click('form button');
+        });
+
+        test('submitting takes user to review screen', async()=>{
+            let confirmationText=await page.getContent('h5');
+
+            expect(confirmationText).toEqual('Please confirm your entries');
+        });
+
+        test('submitting then saving adds blog to blogs page', async()=>{
+            await page.click('button.green');
+            await page.waitFor('.card');
+
+            let title=await page.getContent('.card-title');
+            let content=await page.getContent('p');
+
+            expect(title).toEqual('Test title');
+            expect(content).toEqual('Test content');
+        });
+    });
+
+
+
+});
+
+
+describe('when not logged in', async()=>{
+    test('user cannot create posts', async()=>{
+
+        let status=await page.evaluate(()=>{
+        
+            return fetch('/api/blogs', {
+                method:'POST',
+                credentials:'same-origin',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({title:'Test title', content:'Test content'})
+
+            }).then(res=>res.json()); //fetch returns raw data, needs to be converted to valid JSON
+
+        }
+        
+        );
+
+        expect(status).toEqual({error: 'You must log in!'});
+    });
+
+
+    test('user cannot see existing posts', async()=>{
+        
+        let posts=await page.evaluate(()=>{
+            
+            return fetch('/api/blogs', {
+                method: 'GET',
+                credentials:'same-origin',
+                headers:{
+                    'Content-Type':'application/json'
+                }
+            }).then(res=>res.json());
+        }
+        
+        );
+
+        expect(posts).toEqual({error: 'You must log in!'});
+    });
 });
